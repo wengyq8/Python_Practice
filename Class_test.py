008bc08a2561fbf973ce8ca72abf58555fdbecdb
wengyq8 <wengyq8@gmail.com>
2018-06-24 13:42:09 +0800

    just test push github

--- Class_test.py
+++ Class_test.py
@@ -3,3 +3,6 @@ class MyClass:
         print 'Test push'
 mc = MyClass()
 mc.myNoActionMethod()
+print dir(MyClass)
+print MyClass.__name__
+print MyClass.__doc__
--- classmethod_staticmethod.py
+++ classmethod_staticmethod.py
@@ -1,17 +1,21 @@
+# -*- coding: utf-8 -*-
 class ClassMethod_StaticMethod():
     def foo(self, x):
         print "foo(%s, %s)"%(self, x)
     @classmethod
+    #classmethod默认入参是cls不是self
     def Class_Foo(cls, x):
         print "Class_Foo(%s, %s)"%(cls, x)
     @staticmethod
     def Static_Foo(x):
+    #staticmethod没有隐藏的入参
         print "Static_Foo(%s)"%x
 
 def TestThreeMethod():
     obj = ClassMethod_StaticMethod()
     obj.foo('test')
     obj.Class_Foo('test')
+    #classmethod和staticmethod都可以直接以类名调用方法
     ClassMethod_StaticMethod.Class_Foo('test')
     obj.Static_Foo('test')
     ClassMethod_StaticMethod.Static_Foo('test')
